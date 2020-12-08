281a172f9341a2dd428aa208603a0b004ebe16b8
Shobana <jannah1myaim@gmail.com>
2020-12-08 13:01:06 -0800

    first commit

---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+ $con=mysqli_connect("localhost","root","");
+ if(!$con) 
+	 echo "Not Connected";
+ if(!mysqli_select_db($con,"student"))
+ {
+ 	  echo "DB not conected";
+ }
+ else
+ {	
+   $user=$_POST['user'];
+   $pass=$_POST['pass']; 
+ 
+   $sql="SELECT * FROM register";
+   $result= \mysqli_query($con,$sql);
+   while( $row = mysqli_fetch_assoc($result))
+    {
+    $user= $row['user'];
+    $pass= $row['pass'];
+     if($user == $user && $pass == $pass)
+      {
+    header("Location:http://localhost/student/questions.html");
+
+      }   
+  
+     else
+      {
+     echo "not valid";
+      }
+    }   
+
+  $sql="INSERT INTO login(user,pass) VALUES ('$user','$pass')";
+ } 
+?>
